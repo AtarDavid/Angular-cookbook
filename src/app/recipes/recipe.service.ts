2842961be2ce3395406c7dbc3c71cf860926d71d
Atar David <atar.david@cgi.com>
2020-01-31 11:20:25 +0100

    Using service across components

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [new Recipe('Recipe name', 'This is a description', 'https://via.placeholder.com/50')];
 
     getRecipes() {
