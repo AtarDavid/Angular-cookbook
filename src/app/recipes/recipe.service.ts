70cd18d6794ff908ec569db5f57289a04447fe65
Atar David <atar.david@cgi.com>
2020-01-31 15:18:51 +0100

    Inject shopping list service into recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,25 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
-    private recipes: Recipe[] = [new Recipe('Recipe name', 'This is a description', 'https://via.placeholder.com/50')];
+    private recipes: Recipe[] = [new Recipe('Recipe name', 'This is a description', 'https://via.placeholder.com/50', [
+        new Ingredient('Flour', 2),
+        new Ingredient('Other ingredient', 1)
+    ]
+    )];
+
+    constructor(private shopppingListService: ShoppingListService) { }
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToList(ingredients: Ingredient[]) {
+        this.shopppingListService.addRecipeIngredients(ingredients);
     }
 }
