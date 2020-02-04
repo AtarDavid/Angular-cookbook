a37cfc68e7fb07edf933f6d6a2cf41bd272bb405
Atar David <atar.david@cgi.com>
2020-02-04 15:28:29 +0100

    Add placeholder edit component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeHomeComponent } from './recipes/recipe-home/recipe-home.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/recipes' },
     {
         path: 'recipes', component: RecipesComponent, children: [
             { path: '', component: RecipeHomeComponent },
-            { path: ':id', component: RecipeDetailComponent }
+            { path: 'new', component: RecipeEditComponent },
+            { path: ':id', component: RecipeDetailComponent },
+            { path: ':id/edit', component: RecipeEditComponent }
         ]
     },
     { path: 'shopping-list', component: ShoppingListComponent }
